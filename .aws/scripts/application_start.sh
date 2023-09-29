db346ad11bbdbd20273a7636801bddac1c722444
Tran Ben <vazthunder@gmail.com>
2023-09-29 11:32:23 +0700

    update CodeDeploy scripts

--- .aws/scripts/application_start.sh
+++ .aws/scripts/application_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+aws ecr get-login-password --region AWS_REGION | docker login --username AWS --password-stdin ECR_REGISTRY
 docker pull APP_IMAGE
 docker run -itd --restart unless-stopped -p 3000:3000 --name APP_NAME APP_IMAGE
 
